6949f65ae8fd3550ddca2174bfb209c9b6a7133d
likaihe <1047561425@qq.com>
2016-11-15 14:04:10 +1300

    created a MVC app

---
+++ ChurchMvc/ChurchMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChurchMvc.Startup))]
+namespace ChurchMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
